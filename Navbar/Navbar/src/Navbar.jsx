47e4ba346ca31aedb14289e32a2a46908623c8db
SHAIK SAMEER <shaiksameer967633@gmail.com>
2024-02-17 19:51:57 +0530

    imported images to Navbar component

---
+++ Navbar/Navbar/src/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar.css";
+import logo_light from "./assets/logo-black.png";
+import logo_dark from "./assets/logo-white.png";
+import search_icon_light from "./assets/search-b.png";
+import search_icon_dark from "./assets/search-w.png";
+import toggle_light from "./assets/day.png";
+import toggle_dark from "./assets/night.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src="" alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Features</li>
+        <li>About</li>
+      </ul>
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+        <img src="" alt="" />
+      </div>
+      <img src="" alt="" className="toggle-icon" />
+    </div>
+  );
+};
+
+export default Navbar;
