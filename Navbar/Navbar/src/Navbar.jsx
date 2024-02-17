7902c080b201897a93cf3f6daadafd5f77f75895
SHAIK SAMEER <shaiksameer967633@gmail.com>
2024-02-17 20:55:05 +0530

    designed without any functionality

--- Navbar/Navbar/src/Navbar.jsx
+++ Navbar/Navbar/src/Navbar.jsx
@@ -4,13 +4,13 @@ import logo_light from "./assets/logo-black.png";
 import logo_dark from "./assets/logo-white.png";
 import search_icon_light from "./assets/search-b.png";
 import search_icon_dark from "./assets/search-w.png";
-import toggle_light from "./assets/day.png";
-import toggle_dark from "./assets/night.png";
+import toggle_light from "./assets/night.png";
+import toggle_dark from "./assets/day.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src="" alt="" className="logo" />
+      <img src={logo_light} alt="" className="logo" />
       <ul>
         <li>Home</li>
         <li>Products</li>
@@ -19,9 +19,9 @@ const Navbar = () => {
       </ul>
       <div className="search-box">
         <input type="text" placeholder="Search" />
-        <img src="" alt="" />
+        <img src={search_icon_dark} alt="" />
       </div>
-      <img src="" alt="" className="toggle-icon" />
+      <img src={toggle_light} alt="" className="toggle-icon" />
     </div>
   );
 };
